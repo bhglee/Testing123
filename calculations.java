978a6a0bc6e728eb62e2448fcfab14018f64e815
bhglee <bhglee@users.noreply.github.com>
2016-02-03 11:39:31 -0500

    Create calculations.java

---
+++ calculations.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+Scanner numInput = new Scanner(System.in);
+  
+  int num1;
+  int num2;
+
+public int addingNums (int a, int b)
+{
+  return (a+b)
+  }
+  
+public static void main(String [] args);
+
