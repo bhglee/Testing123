f13346a12043d5743dcc766ecfc6589c804e75ba
bhglee <bhglee@users.noreply.github.com>
2016-02-11 21:57:15 -0500

    Update FizzBuzz2.java

--- FizzBuzz2.java
+++ FizzBuzz2.java
@@ -5,8 +5,14 @@ public class FizzBuzz2 {
 		
 		for (int n=1; n<=100; n++) {
 			
-			System.out.println(n % 3 == 0 || n % 5 == 0 ? (n % 3 == 0 ? "fizz" : "") + (n % 5 == 0 ? "buzz" : "") : n);
-		}
+			if (n % 3 == 0 || n % 5 == 0) {
+				System.out.println ((n % 3 == 0 ? "fizz" : "") + (n % 5 == 0 ? "buzz" : ""));
+		
+				
+			}else{
+				
+				System.out.println(n);
+			}
 	}
 
 }
